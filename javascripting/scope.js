c4a1cc57ba0e745228985578590ee0097ab1ec02
iDebonair <ofemiowolabi@gmail.com>
2023-04-09 15:06:13 +0000

    Completed the objects porting of the javascripting exercise

---
+++ javascripting/scope.js
@@ -0,0 +1,18 @@
+const a = 1;
+const b = 2;
+const c = 3;
+
+(function firstFunction() {
+  const b = 5;
+  const c = 6;
+
+  (function secondFunction() {
+    const b = 8;
+
+    // Access the variables in scope
+    const a = 1;
+    const c = 6;
+
+    console.log(`a: ${a}, b: ${b}, c: ${c}`);
+  })();
+})();
